f5175f39ea58e8b6ad02921bd6db36293328972e
Ewingliang <48040797+Ewingliang@users.noreply.github.com>
2019-03-01 16:15:06 -0500

    dsad

---
+++ re.py
@@ -0,0 +1,10 @@
+import re
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('Phone number found: ' + mo.group())
+
+User name Demo-at-880441974321
+Password QRLwHVxw4OLMDpY5fPpGr+NaAu3wwdn/ur99SMlsnS8=
+
+git clone https://git-codecommit.us-east-2.amazonaws.com/v1/repos/Demo
